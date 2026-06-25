74928b4af43b80374c1bda9cdb8dc4ca8deaf1a6
MatyVic <thexcvb@gmail.com>
2026-06-25 22:45:25 +0300

    Виконання ДЗ 10

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("cheap-books/", views.get_cheap_books, name="cheap_books"),
+    path("search-books/", views.search_books, name="search_books"),
+    path("avg-price/", views.get_avg_price_per_category, name="avg_price"),
+    path("books-by-year/", views.get_books_by_year, name="books_by_year"),
+    path("count-books/", views.count_books_by_price, name="count_books"),
+    ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,34 @@
-
 from django.shortcuts import render
+from django.db.models import Q, Avg, Count
+from shop.models import Book, Category
 
 
-# Create your views here.
+def get_cheap_books(request):
+    cheap_books = Book.objects.filter(price__lt=5)
+    return render(request, "my_template.html", {"books": cheap_books})
+
+
+def search_books(request):
+    search_text = request.GET.get("q", "")
+    amount = request.GET.get("amount", 1)
+
+    search_books_res = Book.objects.filter(
+        Q(author__last_name__icontains=search_text) & Q(amount__gt=amount)
+    ).distinct()
+    return render(request, "my_template.html", {"books": search_books_res})
+
+
+def get_avg_price_per_category(request):
+    avg_price_per_category = Category.objects.annotate(avg_price=Avg("book__price"))
+    return render(request, "avg_price.html", {"categories": avg_price_per_category})
+
+
+def get_books_by_year(request):
+    param_year = request.GET.get("year", 1800)
+    books = Book.objects.filter(published_year__gt=param_year)
+    return render(request, "my_template.html", {"books": books})
+
+
+def count_books_by_price(request):
+    counted_books = Category.objects.annotate(book_count=Count("book"))
+    return render(request, "books_counter.html", {"categories": counted_books})
