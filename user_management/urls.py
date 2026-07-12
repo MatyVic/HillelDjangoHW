fc2c01ac3ab5bd228c055a077349664e10f5621f
MatyVic <thexcvb@gmail.com>
2026-07-12 19:03:26 +0300

    Домашня робота 16.1

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
-from .views import UserFeedBackView, CustomLogoutView
+from .views import UserFeedBackView, CustomLogoutView, user_login, user_register
 
 app_name = "user"
 
 urlpatterns = [
-    path('', views.user_login, name='login'),
-    path('register/', views.user_register, name='register'),
+    path('login/', user_login, name='user_login'),
+    path('register/', user_register, name='register'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('user_feedback/', UserFeedBackView.as_view(), name='user_feedback'),
-    path('user_register/', views.user_register, name='user_register'),
 ]
