dab09a8c39e68f10e427712a181b3da9f872d800
MatyVic <thexcvb@gmail.com>
2026-08-12 11:09:47 +0300

    Завершення перекладу

--- user_management/forms.py
+++ user_management/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
 from user_management.models import CustomUser
+
 
 
 class RegisterForm(UserCreationForm):
@@ -9,7 +11,12 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'birth_date', 'phone_number')
+        labels = {
+            'username': _("Username"),
+            'birth_date': _("Birth date"),
+            'phone_number': _("Phone number"),
+        }
 
 class LoginForm(forms.Form):
-    login = forms.CharField(max_length=250, required=True)
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    login = forms.CharField(max_length=250, required=True, label=_("Login"))
+    password = forms.CharField(widget=forms.PasswordInput, required=True, label=_("Password"))
