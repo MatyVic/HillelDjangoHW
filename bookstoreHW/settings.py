e554741b2371e9c8dd1ae9e96917add392e1a76a
MatyVic <thexcvb@gmail.com>
2026-08-24 15:03:28 +0300

    Додавання тротлінгу

--- bookstoreHW/settings.py
+++ bookstoreHW/settings.py
@@ -251,7 +251,15 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '60/min',
+        'user': '120/min',
+    },
 }
 
 STATICFILES_DIRS = [
