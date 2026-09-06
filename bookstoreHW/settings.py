0f7f44de0ed9ec2dd0d04120b0b0e3d633630049
MatyVic <thexcvb@gmail.com>
2026-09-06 12:53:02 +0300

    Add Cache with Redis

--- bookstoreHW/settings.py
+++ bookstoreHW/settings.py
@@ -301,3 +301,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://username:password@127.0.0.1:6379",
+    }
+}
