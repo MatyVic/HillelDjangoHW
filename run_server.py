a39fff6720a12df857d8eca3df9249b4e814cb72
MatyVic <thexcvb@gmail.com>
2026-09-07 02:22:49 +0300

    updates

--- run_server.py
+++ run_server.py
@@ -1,9 +1,10 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
         "bookstoreHW.asgi:application",
-        host="127.0.0.1",
-        port=8000,
-        reload=True,
+        host=os.environ.get("UVICORN_HOST", "127.0.0.1"),
+        port=int(os.environ.get("UVICORN_PORT", 8000)),
+        reload=os.environ.get("DJANGO_DEBUG", "True") == "True",
     )
